ab579fc3e2c974f36a25dde041444348d6f56c3a
Kulpot <kulpotwahu@gmail.com>
2024-05-19 00:47:06 +0800

    MainFrame.cpp->
     //clearButton = new wxButton(panel, wxID_ANY, "Clear", wxPoint(100, 525), wxSize(100, 35));
     clearButton = new wxButton(panel, wxID_ANY, "Clear");

--- PracticalSizer/MainFrame.cpp
+++ PracticalSizer/MainFrame.cpp
@@ -71,8 +71,9 @@ void MainFrame::CreateControls()
 	addButton = new wxButton(panel, wxID_ANY, "Add");
 	//checkListBox = new wxCheckListBox(panel, wxID_ANY, wxPoint(100, 120), wxSize(600, 400));
 	checkListBox = new wxCheckListBox(panel, wxID_ANY);
-	clearButton = new wxButton(panel, wxID_ANY, "Clear", wxPoint(100, 525), wxSize(100, 35));
-	
+	//clearButton = new wxButton(panel, wxID_ANY, "Clear", wxPoint(100, 525), wxSize(100, 35));
+	clearButton = new wxButton(panel, wxID_ANY, "Clear");
+
 	// (FOR BLURRINESS PROBLEM)->ProjectProperties->Manifest Tool->Input and Output->DPI Awareness->High DPI Aware->Apply
 
 	//headlineText = new wxStaticText(panel, wxID_ANY, "To-Do List");
